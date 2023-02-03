fd647d73c35692d4d3af6a9188f0e3ff83541b9c
Bryon Baker <bryonbakeraus@gmail.com>
2023-02-03 15:25:02 +1100

    Remnoved noisy scripts

--- src/scripts/cleanup-sydney.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-oc delete -f ../src/yaml/app-config-cm.yaml
-oc delete -f ../src/yaml/redis-reader-dep.yaml
-
-skupper delete
-oc delete -f ../skupper-redis/redis-2.yaml
-oc delete -f ../skupper-redis/redis-3.yaml
-oc delete -f ../skupper-redis/redis-4.yaml
--- src/scripts/setup-local-gateway.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-skupper gateway expose skupper-redis-on-prem-server-0 127.0.0.1 6379 26379 --type podman
